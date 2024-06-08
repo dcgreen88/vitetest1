7bfbbdde7e33e4f419776e365d2b364e94b091d3
dcgreen88 <anzianocody@gmail.com>
2024-06-07 21:32:55 -0700

    fixed the position of the modal on larger screens

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,47 +25,50 @@ export default function Modal({ getNewMedia, close, show }) {
 
   return (
     <>
-    {show === 'flex' && <div className="fixed inset-0 bg-black bg-opacity-50 z-40"></div>}
-      <dialog
-        id="MODAL"
-        className={`text-black fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 ${
-          mediaType ? 'bg-yellow-700' : 'bg-green-900'
-        } w-[360px] max-h-[500px] rounded-lg`}
-        open={show === 'flex'}
-      >
-        <h2 className="text-center mb-2 font-semibold text-lg">
-          Add New {mediaType ? 'TV Show' : 'Movie'}
-        </h2>
-        <div id="MAINBUTTONS" className="flex flex-row justify-around">
-          <button
-            onClick={handleSelectMediaType}
-            disabled={!mediaType}
-            className="font-buttons bg-teal-950 border-none rounded-t px-2 text-green-500"
+      {show === 'flex' && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40 flex items-center justify-center">
+          <dialog
+            id="MODAL"
+            className={`text-black z-50 ${
+              mediaType ? 'bg-yellow-700' : 'bg-green-900'
+            } w-full min-w-[320px] max-w-[360px] max-h-[500px] rounded-lg`}
+            open={show === 'flex'}
           >
-            Movie
-          </button>
-          <button
-            onClick={handleSelectMediaType}
-            disabled={mediaType}
-            className="font-buttons bg-teal-950 border-none rounded-t px-2 text-yellow-500"
-          >
-            TV Show
-          </button>
+            <h2 className="text-center mb-2 font-semibold text-lg">
+              Add New {mediaType ? 'TV Show' : 'Movie'}
+            </h2>
+            <div id="MAINBUTTONS" className="flex flex-row justify-around">
+              <button
+                onClick={handleSelectMediaType}
+                disabled={!mediaType}
+                className="font-buttons bg-teal-950 border-none rounded-t px-2 text-green-500"
+              >
+                Movie
+              </button>
+              <button
+                onClick={handleSelectMediaType}
+                disabled={mediaType}
+                className="font-buttons bg-teal-950 border-none rounded-t px-2 text-yellow-500"
+              >
+                TV Show
+              </button>
+            </div>
+            <MovieSelector
+              searchType={mediaType}
+              getMediaObject={forwardNewMediaObject}
+              close={handleClose}
+            />
+            <div id="CLOSE" className="flex flex-col items-center pb-1 pt-1">
+              <button
+                onClick={handleClose}
+                className="my-[4px] w-14 font-buttons bg-teal-950 text-red-500 rounded py-[1px]"
+              >
+                Close
+              </button>
+            </div>
+          </dialog>
         </div>
-        <MovieSelector
-          searchType={mediaType}
-          getMediaObject={forwardNewMediaObject}
-          close={handleClose}
-        />
-        <div id="CLOSE" className="flex flex-col items-center pb-1 pt-1">
-          <button
-            onClick={handleClose}
-            className="my-[4px] w-14 font-buttons bg-teal-950 text-red-500 rounded py-[1px]"
-          >
-            Close
-          </button>
-        </div>
-      </dialog>
+      )}
     </>
   );
 }
